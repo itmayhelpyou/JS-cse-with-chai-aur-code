bafdf591fceb2437431c47fcff6d70020e0842c4
Hariprasad Ghimire <98560159+itmayhelpyou@users.noreply.github.com>
2024-03-04 09:46:27 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 0364
+let accountEmail = "harigee@hotmail.accountEmail"
+var accountPassword = "12345"
+accountCity = "Ilam"
+
+// accountId = 2 // const can't be changed once it is decleared. 
+
+console.log(accountId);
